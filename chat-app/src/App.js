0d1f5598b8c6d836be6cd15bbef810a4c32b5fee
Aritra Nag <aritranag8835@gmail.com>
2024-09-24 01:36:35 +0530

    added signup ,login page and dark mode button

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,13 @@
 import ToggleColorMode from "./components/ToggleColorMode";
+import Views from "./components/Views.jsx";
 
 function App() {
   return (
-
+    <div>
+      <Views />
       <ToggleColorMode />
-
+      
+</div>
   );
 }
 
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import App from './App.js';
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import theme from './theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter>
   <ChakraProvider theme={theme}>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>
+  </BrowserRouter>
     
   </React.StrictMode>
 );
