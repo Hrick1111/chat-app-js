cd39e6443694826e647e5cad541cbd2b7a825dca
Aritra Nag <aritranag8835@gmail.com>
2024-09-23 16:19:24 +0530

    first commit

---
+++ chat-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import App from './App.js';
+import { ColorModeScript } from "@chakra-ui/color-mode";
+import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom';
+import theme from './theme.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </ChakraProvider>
+    
+  </React.StrictMode>
+);
+
