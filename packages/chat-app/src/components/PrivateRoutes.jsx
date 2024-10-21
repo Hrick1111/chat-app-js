52499484acfa4554365106b732041cb63f19ee82
Aritra Nag <aritranag8835@gmail.com>
2024-10-21 18:13:26 +0530

    added chatbox and changed routes

--- packages/chat-app/src/components/PrivateRoutes.jsx
+++ packages/chat-app/src/components/PrivateRoutes.jsx
@@ -1,13 +1,12 @@
 import { useContext } from "react";
 import { AccountContext } from "./AccountContext";
-const { Outlet, Navigate } = require("react-router");
 
+const { Outlet, Navigate } = require("react-router");
 
 const useAuth = () => {
   const { user } = useContext(AccountContext);
   return user && user.loggedIn;
 };
-
 
 const PrivateRoutes = () => {
   const isAuth = useAuth();
